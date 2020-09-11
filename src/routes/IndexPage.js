32710702e15334403976cc77bb5b2affb33af03e
hllinc <hllinc@163.com>
2020-09-11 14:37:09 +0800

    添加首页导航。

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
+import {Link} from 'dva/router';
 
 function IndexPage() {
   return (
@@ -11,6 +12,17 @@ function IndexPage() {
         <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
         <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
       </ul>
+      <nav>
+        <div>导航</div>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
