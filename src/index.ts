f6ac3da2222112710987f792367487cf572e6f2b
hllinc <hllinc@163.com>
2020-12-23 18:55:23 +0800

    添加 sentry 异常监控。

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,21 @@ import dva from 'dva';
 import './index.css';
 import {createBrowserHistory} from "history";
 
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "http://935f423fb3a54b959b8f2873956d68c8@127.0.0.1:9000/3",
+  autoSessionTracking: true,
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
+
 const createHistory = createBrowserHistory();
 
 const app = dva({
